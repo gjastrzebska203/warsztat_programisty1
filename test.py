091e7f19b2e10783468865566a07d360c1058aef
gjastrzebska203 <g.jastrzebska.203@studms.ug.edu.pl>
2024-01-25 19:28:53 +0100

    zmiany w test.py

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ class TestMojProgram(unittest.TestCase):
     def test_sum(self):
         self.assertEqual(sum(2, 4), 6)
         self.assertEqual(sum(-10, 10), 0)
-        self.assertEqual(sum(1, 5), 10)
+        self.assertEqual(sum(1, 5), 6)
 
 if __name__ == '__main__':
     unittest.main()
