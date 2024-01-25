9ce336b7c756d29e6e3c9040ab09ffb8f4a48d1d
gjastrzebska203 <g.jastrzebska.203@studms.ug.edu.pl>
2024-01-25 18:52:47 +0100

    dalsze proby

--- dec_bin.py
+++
@@ -1,5 +0,0 @@
-def dec_to_bin(number):
-    if number <= 1:
-        return str(number)
-    else:
-        return dec_to_bin(number // 2) + str(number % 2)
--- symbol_newtona.py
+++
@@ -1,5 +0,0 @@
-def symbol_newtona(n, k):
-    if k == 0 or n == k:
-        return 1
-    else:
-        return symbol_newtona(n - 1, k - 1) + symbol_newtona(n - 1, k)
